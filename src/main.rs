b3bc81c30a0743f8575f4c51303d61d7311fbe62
Victor <victor@caern.de>
2021-08-23 21:58:06 +0200

    Removed old tutorial code.

--- src/main.rs
+++ src/main.rs
@@ -13,41 +13,17 @@ use bevy::prelude::*;
 
 
 //=============================================================================
-// Components
-
-
-struct Person;
-
-
-struct Name( String );
-
-
-
-
-//=============================================================================
 // Plugins
 
 
-pub struct HelloPlugin;
+pub struct ComputerPlugin;
 
-impl Plugin for HelloPlugin {
+impl Plugin for ComputerPlugin {
 	fn build( &self, app: &mut AppBuilder ) {
-		app.insert_resource( GreetTimer( Timer::from_seconds( 2.0, true ) ) )
-			.add_startup_system( add_people.system() )
-			.add_startup_system( setup.system() )
-			.add_system( greet_people.system() )
+		app.add_startup_system( setup.system() )
 			.add_system( animate.system() );
 	}
 }
-
-
-
-
-//=============================================================================
-// Resources
-
-
-struct GreetTimer( Timer );
 
 
 
@@ -88,28 +64,6 @@ fn setup(
 }
 
 
-fn add_people( mut commands: Commands ) {
-	commands.spawn().insert( Person ).insert( Name( "Elaina Proctor".to_string() ) );
-	commands.spawn().insert( Person ).insert( Name( "Renzo Hume".to_string() ) );
-	commands.spawn().insert( Person ).insert( Name( "Zayna Nieves".to_string() ) );
-}
-
-
-fn greet_people(
-	time: Res<Time>,
-	mut timer: ResMut<GreetTimer>,
-	query: Query<&Name, With<Person>>
-) {
-	// update our timer with the time elapsed since the last update
-	// if that caused the timer to finish, we say hello to everyone
-	if timer.0.tick(time.delta()).just_finished() {
-		for name in query.iter() {
-			println!( "Hello {}!", name.0 );
-		}
-	}
-}
-
-
 fn animate(time: Res<Time>, mut query: Query<&mut Transform, With<Text>>) {
 	// Moving the text slowly in a circle.
 	for mut transform in query.iter_mut() {
@@ -128,6 +82,6 @@ fn animate(time: Res<Time>, mut query: Query<&mut Transform, With<Text>>) {
 fn main() {
 	App::build()
 		.add_plugins( DefaultPlugins )
-		.add_plugin( HelloPlugin )
+		.add_plugin( ComputerPlugin )
 		.run();
 }
