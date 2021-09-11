4e6f6da612360d1bbcdb027cdfa2df9ffe2ac234
Victor <victor@caern.de>
2021-09-12 00:23:48 +0200

    Removed useless code.

--- src/main.rs
+++ src/main.rs
@@ -59,8 +59,7 @@ impl Plugin for ComputerPlugin {
 					.with_system( computer::update_usage_smooth.system() ),
 			)
 			.add_system( ui::observe_button.system() )
-			.add_system( computer::update_usage.system() )
-			.add_system( animate.system() );
+			.add_system( computer::update_usage.system() );
 	}
 }
 
@@ -82,7 +81,6 @@ fn setup(
 
 	// Load sprite
 	let texture_handle = asset_server.load( "Processor.png" );
-	commands.spawn_bundle( OrthographicCameraBundle::new_2d() );
 	commands.spawn_bundle( SpriteBundle {
 		material: materials.add( texture_handle.into() ),
 		..Default::default()
@@ -100,32 +98,6 @@ fn setup(
 	commands.spawn_bundle( (
 		ComputerSchedule::from_template( "Family" ),
 	) );
-
-	// 2D Text
-	commands.spawn_bundle( Text2dBundle {
-		text: Text::with_section(
-			"Simple text message.",
-			TextStyle {
-				font: asset_server.load( "fonts/Orbitron/Orbitron-Regular.ttf" ),
-				font_size: 60.0,
-				color: Color::WHITE,
-			},
-			TextAlignment {
-				vertical: VerticalAlign::Center,
-				horizontal: HorizontalAlign::Center,
-			},
-		),
-		..Default::default()
-	} );
-}
-
-
-fn animate(time: Res<Time>, mut query: Query<&mut Transform, With<Text>>) {
-	// Moving the text slowly in a circle.
-	for mut transform in query.iter_mut() {
-		transform.translation.x = 100.0 * time.seconds_since_startup().sin() as f32;
-		transform.translation.y = 100.0 * time.seconds_since_startup().cos() as f32;
-	}
 }
 
 
